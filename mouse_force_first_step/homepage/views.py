6b81be543428f79896f969727ef81a613d52d41c
Slicari <goreaionut970@gmail.com>
2025-04-16 21:38:04 +0300

    Update static files and styles

--- mouse_force_first_step/homepage/views.py
+++ mouse_force_first_step/homepage/views.py
@@ -1,4 +1,9 @@
+from mouse_force_first_step.mycookieprivacy.models import UserCookies
+from django.utils.timezone import now
 from django.shortcuts import render
-
 def index(request):
-    return render(request, 'index.html')
+    user_ip = request.META.get('REMOTE_ADDR')
+    already_set = UserCookies.objects.filter(user_ip=user_ip).exists()
+    return render(request, 'index.html', {'show_cookie_banner': True})
+# return render(request, 'index.html', {'show_cookie_banner': not already_set})
+
